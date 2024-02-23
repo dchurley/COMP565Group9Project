4702ebb8d75b464b5188ac315fc21453c941727c
dchurley <136537343+dchurley@users.noreply.github.com>
2024-02-23 13:14:37 -0800

    made projectiles destroy off screen

--- Assets/Scripts/PlayerProjectile.cs
+++ Assets/Scripts/PlayerProjectile.cs
@@ -6,7 +6,7 @@ public class PlayerProjectile : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    public GameObject go;
+    //public GameObject go;
     void Start()
     {
         
@@ -15,7 +15,19 @@ public class PlayerProjectile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!IsVisible())
+        {
+            // Destroy the projectile
+            Destroy(gameObject);
+        }
+    }
 
-        Instantiate(go);
+    private bool IsVisible()
+    {
+        // Get the screen boundaries
+        Vector3 screenPoint = Camera.main.WorldToViewportPoint(transform.position);
+
+        // Check if the projectile is within the viewport
+        return screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
     }
 }
