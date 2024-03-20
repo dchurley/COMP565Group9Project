0ff1bdf127ea0dee570a9be0a50bca2f80982456
dchurley <136537343+dchurley@users.noreply.github.com>
2024-03-20 14:43:21 -0700

    working on boss bar

--- Assets/Scripts/BossBar.cs
+++ Assets/Scripts/BossBar.cs
@@ -1,24 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BossBar : MonoBehaviour
 {
     // Start is called before the first frame update
     public float scale = 1.0f;
-    private float sizeX;
-    private float sizeY;
+    public Image i;
 
     void Start()
     {
-        Transform orig = transform.GetChild(0);
-        sizeX = orig.localScale.x;
-        sizeY = orig.localScale.y;
+
     }
 
     void resizeBar()
     {
-        gameObject.transform.GetChild(0).localScale = new Vector3(sizeX * scale, sizeY, 1);
+        i.fillAmount = scale;
     }
 
     // Update is called once per frame
