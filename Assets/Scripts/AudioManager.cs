a31e80224f4127d07d437f6e88aad8461a928d9c
dahsanbeasley <89285519+dahsanbeasley@users.noreply.github.com>
2024-05-11 02:32:28 -0700

    audio system fixed

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,8 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameAudioManager : MonoBehaviour
 {
     [SerializeField] AudioSource musicSource;
+    [SerializeField] Slider volumeSlider;
 
     public AudioClip sound;
     public AudioClip winMusic;
@@ -11,11 +15,19 @@ public class GameAudioManager : MonoBehaviour
     {
         musicSource.clip = sound;
         musicSource.Play();
+        volumeSlider.onValueChanged.AddListener(delegate { updateVolumeSlider(); });
+        musicSource.volume = PlayerPrefs.GetFloat("volumeSlider", 0.75f);
     }
 
     public void PlayWinMusic()
     {
         musicSource.clip = winMusic;
         musicSource.Play();
+    }
+
+    void updateVolumeSlider()
+    {
+        musicSource.volume = volumeSlider.value;
+        PlayerPrefs.SetFloat("volumeSlider", volumeSlider.value);
     }
 }
---
+++ Assets/Scripts/SliderVolume.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderVolume : MonoBehaviour
+{
+    public Slider mSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mSlider.value = PlayerPrefs.GetFloat("volumeSlider", 0.75f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
